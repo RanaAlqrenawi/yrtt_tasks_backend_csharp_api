8946d58917264588c962b8de677663276f8db501
Rana Alqrenawi <rana@gazaskygeeks.com>
2021-03-10 19:51:49 +0000

    Creat a list of tasks and return them

--- Handler.cs
+++ Handler.cs
@@ -1,31 +1,45 @@
 using Amazon.Lambda.Core;
+using System.Collections;
 
-[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response Hello(Request request)
-       {
-           return new Response("Go Serverless v1.0! Rana, Your YRTT function executed successfully!", request);
-       }
+        public ArrayList Tasks(Request task)
+        {
+            ArrayList tasks = new ArrayList();
+            Task t1 = new Task("1234", " Make milk!", false);
+            Task t2 = new Task("5678", " Change the nappy!", true);
+            Task t3 = new Task("9101", " Clean the kitchen!", false);
+
+            tasks.Add(t1);
+            tasks.Add(t2);
+            tasks.Add(t3);
+
+            return tasks;
+        }
     }
 
-    public class Response
+    public class Task
     {
-      public string Message {get; set;}
-      public Request Request {get; set;}
+        public string TaskId { get; }
+        public string Description { get; }
 
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
-      }
+        public bool Completed { get; }
+
+        public Task(string taskId, string description, bool completed)
+        {
+            TaskId = taskId;
+            Description = description;
+            Completed = completed;
+        }
     }
 
     public class Request
     {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
+        public string Key1 { get; set; }
+        public string Key2 { get; set; }
+        public string Key3 { get; set; }
     }
 }
