26dd0ffe7aa978d98187c88470d20909c783a6cd
Rana Alqrenawi <rana@gazaskygeeks.com>
2021-03-11 16:17:09 +0000

    Path parameters to denote the required task

--- Handler.cs
+++ Handler.cs
@@ -1,21 +1,32 @@
 using Amazon.Lambda.Core;
 using System.Collections;
+using Amazon.Lambda.APIGatewayEvents;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-        public ArrayList getTasks(Request task)
+        public ArrayList getTasks(APIGatewayProxyRequest request)
         {
-            ArrayList tasks = new ArrayList();
-            Task t1 = new Task("1234", " Make milk!", false);
-            Task t2 = new Task("5678", " Change the nappy!", true);
-            Task t3 = new Task("9101", " Clean the kitchen!", false);
+            string userId = request.PathParameters["userId"];
+            LambdaLogger.Log("Getting tasks for:" + userId);
 
-            tasks.Add(t1);
-            tasks.Add(t2);
-            tasks.Add(t3);
+            ArrayList tasks = new ArrayList();
+
+            if (userId == "1234")
+            {
+                Task t1 = new Task("1234", " Make milk!", false);
+                tasks.Add(t1);
+            }
+            else
+            {
+                Task t2 = new Task("5678", " Change the nappy!", true);
+                Task t3 = new Task("9101", " Clean the kitchen!", false);
+
+                tasks.Add(t2);
+                tasks.Add(t3);
+            }
 
             return tasks;
         }
